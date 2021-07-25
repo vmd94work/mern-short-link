03939797e88c23fb6972bc137c1f48a75098ed56
unknown <vmd94.work@gmai.com>
2021-07-25 23:06:38 +0300

    initial

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const LinkCard = ({ link }) => {
+  return (
+    <>
+      <h2>Ссылка</h2>
+      <p>
+        Ваша Ссылка:
+        <a href={link.to} target="_blank" rel="noopener noreferrer">
+          {link.to}
+        </a>
+      </p>
+      <p>
+        Откуда:
+        <a href={link.from} target="_blank" rel="noopener noreferrer">
+          {link.from}
+        </a>
+      </p>
+      <p>
+        К-во кликов по ссылке:
+        <strong>{link.clicks}</strong>
+      </p>
+      <p>
+        Дата создания:{" "}
+        <strong>{new Date(link.date).toLocaleDateString()}</strong>
+      </p>
+    </>
+  );
+};
---
+++ client/src/pages/LinksPage.js
@@ -0,0 +1,29 @@
+import React, { useState, useContext, useCallback, useEffect } from "react";
+import { Loader } from "../components/Loader";
+import { AuthContext } from "../context/AuthContext";
+import { useHttp } from "../hooks/http.hook";
+import { LinksList } from "../components/LinksList";
+
+export const LinksPage = () => {
+  const [links, setLinks] = useState([]);
+  const { loading, request } = useHttp();
+  const { token } = useContext(AuthContext);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request("/api/link", "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched);
+    } catch (e) {}
+  }, [token, request]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+  return <div>{!loading && <LinksList links={links} />}</div>;
+};
